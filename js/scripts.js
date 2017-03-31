f2981ccde1d8b98e1a7635c730cba0f636fcf881
James Higgins <james.milan.higgins@gmail.com>
2017-03-31 10:35:26 -0700

    add js to make alerts pop up 5 different times, 1, 2, ping, 4, pong, but with numbers defined in the top as an array of 5 set numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,46 @@
 //Back-end Logic
-
-
-var pingPong = function(input){
-  return input;
-}
+var numbers = ["1", "2", "3", "4", "5"];
 
 
 
+numbers.forEach(function(number) {
+  if ((number % 3)=== 0) {
+    number = "ping";
+  }  else if ((number % 5) === 0) {
+    number = "pong";
+  }  alert(number);
+});
 
 
+
+// var pingPong = function(input){
+// var numbers = parseInt(pingPong);
+// var updated = numbers -1;
+//
+//
+// return updated;
+// }
+
+  //the code I want
+  // for(var i=1; i <= input; i++) {
+  //   number = i;
+    // if ((number % 3)=== 0) {
+    //   number = "ping";
+    //   alert(numbers);
+    // } else if ((number % 5) === 0) {
+    //   number = "pong";
+  //   } else if ((number % 3)=== 0 && (number % 5) === 0) {
+  //     number = "ping-pong";
+  //   }
 
 //Front-end Logic
 $(document).ready(function(){
   $("#form").submit(function(event) {
+
     event.preventDefault();
-    var input = $("#userInput").val();
-    $("#results").text(input);
+    var input = $("#numberInput").val();
+    $("#output").text(input);
+    });
+
   });
-});
+// });
