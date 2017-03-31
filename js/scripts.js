c214ea5b78023942b68a051fb32f38192e58eb27
James Higgins <james.milan.higgins@gmail.com>
2017-03-31 08:42:21 -0700

    add js to make form return and display numbers that the user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 //Back-end Logic
 
 
-
+var pingPong = function(input){
+  return input;
+}
 
 
 
@@ -9,3 +11,10 @@
 
 
 //Front-end Logic
+$(document).ready(function(){
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    var input = $("#userInput").val();
+    $("#results").text(input);
+  });
+});
